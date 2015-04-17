72c50098750af53d71f39d34977afb66aeb09d68
Peter Tran <petah.tran@utoronto.ca>
2015-04-17 17:06:34 -0400

    Started parsing directory to save in db.

--- olrcdb.py
+++ olrcdb.py
@@ -15,14 +15,57 @@ class DatabaseConnection(object):
                 host=os.environ["MYSQL_HOST"],
                 user=os.environ["MYSQL_USER"],
                 passwd=os.environ["MYSQL_PASSWD"],
+                db=os.environ["MYSQL_DB"],
             )
+            self.cursor = self.db.cursor()
         except KeyError:
             sys.exit("Please make sure all required environment variables"
                      " are set:\n$MYSQL_HOST\n$MYSQL_DB\n$MYSQL_USER\n"
                      "$MYSQL_PASSWD\n")
         except MySQLdb.Error, e:
-            sys.exit("ERROR %d IN CONNECTION: %s".format(e.args[0], e.args[1]))
+            sys.exit("ERROR {0} IN CONNECTION: {1}".format(e.args[0], e.args[1]))
 
     def get_cursor(self):
         '''Return a cursor for the database.'''
-        return self.db.cursor()
+        return self.cursor
+
+    def create_table(self, table_name):
+        '''Given a table_name, create a table in the database.
+
+        Schema for the table will be:
+        CREATE TABLE MyGuests (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            firstname VARCHAR(30) NOT NULL,
+            lastname VARCHAR(30) NOT NULL,
+            email VARCHAR(50),
+            reg_date TIMESTAMP
+            )
+        '''
+
+        query = "CREATE TABLE {0} ( \
+            path VARCHAR(1000),\
+            uploaded BOOL DEFAULT '0'\
+            )".format(table_name)
+
+        try:
+            self.cursor.execute(query)
+        except MySQLdb.Error, e:
+            sys.exit("ERROR {} IN TABLE CREATION: {}".format(
+                e.args[0],
+                e.args[1]
+            ))
+
+    def insert_path(self, path, table_name):
+        '''Insert the given path to the table_name.'''
+
+        query = "INSERT INTO {0} (path) VALUES ('{1}');".format(
+            table_name,
+            path
+        )
+        try:
+            self.cursor.execute(query)
+        except MySQLdb.Error, e:
+            sys.exit("ERROR {0} IN INSERT: {q}".format(
+                e.args[0],
+                e.args[1]
+            ))
---
+++ prepareupload.py
@@ -0,0 +1,54 @@
+import sys
+import olrcdb
+import os
+
+# Globals
+COUNT = 0
+
+
+class FileParser(object):
+    '''Object used to parse through a directory for all it's files. Collects
+    the paths of all the files and stores a record of these in a new table in
+    the database.
+
+    The Schema of the database is:
+
+    NewTable(path, uploaded=false)
+    '''
+
+    def __init__(self, directory, table_name):
+        self.directory = directory
+        self.table_name = table_name
+
+
+def prepare_upload(connect, directory, table_name):
+    '''Given a database connection, directory and table_name,
+    -Create the table in the database
+    -populate the table with (path, uploaded=false)
+    where each path is a file in the given directory.'''
+
+    # Create table
+
+    for filename in os.listdir(directory):
+
+        file_path = os.path.join(directory, filename)
+
+        # Add file name to the list.
+        if os.path.isfile(file_path):
+            connect.insert_path(table_name, file_path)
+        else:
+            prepare_upload(connect, directory, table_name)
+
+
+if __name__ == "__main__":
+
+    # Check for proper parameters
+    if len(sys.argv) != 3:
+        sys.stderr.write(
+            'Usage: python prepareupload.py path-to-drive table-name\n'
+        )
+        sys.exit(1)
+
+    connect = olrcdb.DatabaseConnection()
+    connect.create_table(sys.argv[2])
+    prepare_upload(connect, sys.argv[1], sys.argv[2])
